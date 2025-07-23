97bbbc9757d0e9d633e5fd890481b0f9ee9cf828
AmanDwivedi9335 <amandwdi@gmail.com>
2025-07-23 14:39:06 +0530

    picture8 removed

--- components/HomeNew/NewsletterSection.jsx
+++ components/HomeNew/NewsletterSection.jsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Picture from "@/public/images/NewHome/picture8.png";
 import { Inter, Outfit } from "next/font/google";
 import { useRouter } from "next/navigation";
 
